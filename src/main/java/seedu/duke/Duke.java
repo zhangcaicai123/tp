9cb77fa4694a66e8320eec5c8cfcb060cf60b47b
Geng Yu <e0496102@u.nus.edu>
2020-10-10 20:35:59 +0800

    add_module_and_parser_class_v3

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -11,19 +11,15 @@ public class Duke {
     void run() {
 
         while (!parser.isExit) {
+            userCommand = in.nextLine();
+            parser.parse(userCommand);
 
-            if (in.hasNextLine()) {
-
-                userCommand = in.nextLine();
-                parser.parse(userCommand);
-
-            }
         }
 
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         System.out.println("Hello, this is CEGMods! What can I do for you?");
         new Duke().run();
