b85e3f072d34d7772e89b62b335c4bfada8ffdd7
Geng Yu <e0496102@u.nus.edu>
2020-10-10 14:54:04 +0800

    add_module_and_parser_class

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,19 +3,22 @@ package seedu.duke;
 import java.util.Scanner;
 
 public class Duke {
-    /**
-     * Main entry-point for the java.duke.Duke application.
-     */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+    Scanner in = new Scanner(System.in);
+    Parser p = new Parser();
+
+    void run() {
+        while(!p.isExit) {
+            String userCommand = in.nextLine();
+            p.parse(userCommand);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, this is CEGMods! What can I do for you?");
+        new Duke().run();
+
     }
 }
---
+++ src/main/java/seedu/duke/Module.java
@@ -0,0 +1,22 @@
+package seedu.duke;
+
+public class Module {
+    String modName;
+    String lecSlot;
+    String tutSlot;
+    String labSlot;
+
+    public Module(String mod, String lec, String tut, String lab) {
+        this.modName = mod;
+        this.lecSlot = lec;
+        this.tutSlot = tut;
+        this.labSlot = lab;
+    }
+
+    public Module(String mod, String lec, String tut) {
+        this.modName = mod;
+        this.lecSlot = lec;
+        this.tutSlot = tut;
+    }
+
+}
