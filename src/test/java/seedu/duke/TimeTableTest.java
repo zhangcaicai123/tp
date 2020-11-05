0b624037392256a2dfd30e1d548800cd3cac9bc1
NAOMI\lemonhyuuga <lemonhyuuga@gmail.com>
2020-11-05 21:42:03 +0800

    add some timetabletests

--- src/test/java/seedu/duke/TimeTableTest.java
+++ src/test/java/seedu/duke/TimeTableTest.java
@@ -1,7 +1,11 @@
 package seedu.duke;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TimeTableTest {
 
@@ -11,4 +15,23 @@ public class TimeTableTest {
         assertTrue(true);
     }
 
+    @Test
+    void isModuleAdded() {
+        TimeTable table = new TimeTable();
+        String moduleCode = "CS2113";
+        assertEquals(TimeTable.isModuleAdded(moduleCode),false);
+    }
+
+    @Test
+    void addModule_noSuchModule_exceptionThrown() {
+        TimeTable table = new TimeTable();
+        String command = "add mod/CS2114";
+        try {
+            table.addModule(command);
+        } catch (NoSuchElementException | IOException e) {
+            assertEquals("_______________________________________________________\n"
+                    + "There is no such module.\n"
+                    + "_______________________________________________________", e.getMessage());
+        }
+    }
 }
